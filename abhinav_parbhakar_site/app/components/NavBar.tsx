95512b2adf03578ee5eff909af20a519156406f3
Abhinav parbhakar <abhinav.parbhakar302@gmail.com>
2023-06-24 10:15:30 -0600

    editing the home page

--- abhinav_parbhakar_site/app/components/NavBar.tsx
+++ abhinav_parbhakar_site/app/components/NavBar.tsx
@@ -11,17 +11,17 @@ export default function NavBar(){
     return(
     <div className="bg-slate-300 flex flex-col justify-center drop-shadow-md">
         <div className="md:hidden flex justify-center ">
-            <button className="rounded-md border-none" onClick={setBurger}>
+            <button className="rounded-md border-2 border-white-300 drop-shadow-lg" onClick={setBurger}>
                 {burgerState ? <img className="max-h-9 max-w-9"src="/hamburg-open.png" alt="" />:<img className="max-h-9 max-w-9"src="/hamburg-close.png" alt=""></img>}
             </button>
         </div>
         <div className="flex justify-center">
         {!burgerState ? 
             (<div className="flex flex-col md:hidden justify-center">
-                <Link href="/" className="text-orange-400 hover:text-orange-300  font-mono" onClick={setBurger}>Home</Link>
-                <Link href="/Education"className="text-orange-400 hover:text-orange-300  font-mono" onClick={setBurger}>Education</Link>
-                <Link href="/Experience" className="text-orange-400 hover:text-orange-300  font-mono" onClick={setBurger}>Experience</Link>
-                <Link href="/Projects" className="text-orange-400 hover:text-orange-300  font-mono" onClick={setBurger}>Projects</Link>
+                <Link href="/" className="text-orange-400 hover:text-orange-300  font-mono px-8" onClick={setBurger}>Home</Link>
+                <Link href="/Education"className="text-orange-400 hover:text-orange-300  font-mono px-3" onClick={setBurger}>Education</Link>
+                <Link href="/Experience" className="text-orange-400 hover:text-orange-300  font-mono px-2" onClick={setBurger}>Experience</Link>
+                <Link href="/Projects" className="text-orange-400 hover:text-orange-300  font-mono px-4" onClick={setBurger}>Projects</Link>
             </div>):<></>}
         </div>
         <div className="flex justify-center">
--- abhinav_parbhakar_site/app/page.tsx
+++ abhinav_parbhakar_site/app/page.tsx
@@ -3,9 +3,15 @@ export default function Home() {
   return (
     <>
     <title>Abhinav Parbhakar Portfolio</title>
-    <div className="flex flex-col">
-      <div className="flex flex-col md:flex-row">
-          <h1>Who is <p className="text-orange-400">Abhinav Parbhakar</p></h1>
+    <div className="flex flex-col mt-3">
+      <div className="flex flex-col md:flex-row md:justify-center">
+          <h1 className='flex flex-col mx-3'>Who is <p className="text-7xl font-mono text-orange-400"> Abhinav Parbhakar</p></h1>
+          <img src="/homePagePic.png" alt="" className='max-w-lg max-h-80'/>
+      </div>
+      <div className='flex justify-center'>
+        <p className='font-sans'>Currently enrolled at University of Alberta, entering his final year in the BS Computing Science Program,
+          <style className='text-orange-400'>Abhinav Parbhakar</style> has experience with front-end and back-end development.
+        </p>
       </div>
     </div>
     </>
