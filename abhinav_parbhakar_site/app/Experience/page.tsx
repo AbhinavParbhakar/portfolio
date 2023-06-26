da020d5cbe5890d2a2beb252e33abd78fb713cde
Abhinav parbhakar <abhinav.parbhakar302@gmail.com>
2023-06-26 10:29:26 -0600

    everything is working now'

--- abhinav_parbhakar_site/app/Experience/page.tsx
+++ abhinav_parbhakar_site/app/Experience/page.tsx
@@ -2,16 +2,13 @@ import { post } from "../interface"
 
 export default async function Experience(){
     let items:Array<post>= await name()
-    console.log(items)
     return(
     <div>
         <h1>
             Work Experience
         </h1>
         <p>
-            {items.map((item:post) => {
-                return <p>item.description</p>
-            })}
+            {items[0].description}
         </p>
     </div>
     )
@@ -20,7 +17,7 @@ export default async function Experience(){
 async function name() {
     const request:object = {"type":"work"}
     let response = await fetch("https://7buwhdoti2.execute-api.us-east-2.amazonaws.com/Alpha",{body:JSON.stringify(request),mode:'cors',headers:{'content-type':'application/json'},next:{revalidate:60},method:'POST'})
-    let data = response.json()
-    console.log(data)
-    return data
+    let data = await response.json()
+    const body:Array<post> = data.body
+    return body
 }
