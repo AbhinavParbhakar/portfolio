01b4483e059b7b3d6c8d4e16f7aa725644e7e217
Abhinav parbhakar <abhinav.parbhakar302@gmail.com>
2023-06-28 17:34:54 -0600

    edited the breadcrumbs

--- abhinav_parbhakar_site/app/components/BreadCrumbs.tsx
+++ abhinav_parbhakar_site/app/components/BreadCrumbs.tsx
@@ -9,32 +9,24 @@ interface pathMapping{
 
 export default function BreadCrumbs(){
     var pathList:Array<string> = usePathname().split("/")
+    
+    if (pathList[pathList.length - 1] == ""){
+        pathList.pop()
+    }
 
     var count = 0
+    var currentPath = ""
 
     let pathEmoji:pathMapping = {"":"🏠","Projects":"👨‍💻","Experience":"🧑‍💼","Education":"📖"}
-    let pathURL:pathMapping = {"":"/","Projects":"/Projects","Experience":"/Experience","Education":"/Education"}
+    
 
     return(
     <div className="flex justify-center mt-2 md:hidden">
         {pathList.map((path:string)=>{
-            if (count < pathList.length){
-                if (path == "" && count == 0){
-                    count++
-                    let currentPath = "Home"
-                    return <Link href= {pathURL[path]}>{pathEmoji[path]} {currentPath} </Link>
-                }else if (path != ""){
-                    count++
-                    if (count > 0){
-                        return <Link href= {pathURL[path]}>&nbsp;&#8594; {pathEmoji[path]} {path}</Link>
-                    }else{
-                        return <Link href= {pathURL[path]}>{pathEmoji[path]} {path}</Link>
-                    }   
-                }
-            }else{
-                return <Link href={usePathname()}>{pathList[pathList.length - 1]}</Link>
-            }
-        })}
+            currentPath  = currentPath + "/" + pathList[count]
+            return <p>{pathEmoji[pathList[count]]}&nbsp;<Link href={currentPath}>&nbsp;&#8594;</Link></p>
+            })
+        }
     </div>
     )
 }
