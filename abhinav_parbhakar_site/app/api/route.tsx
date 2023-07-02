d0f76c545b642e2171fd8a7669438293b3c9e48c
Abhinav parbhakar <abhinav.parbhakar302@gmail.com>
2023-07-02 17:20:04 -0600

    Edited the email sending page

--- abhinav_parbhakar_site/app/api/route.tsx
+++ abhinav_parbhakar_site/app/api/route.tsx
@@ -21,19 +21,29 @@ function createEmailText(data:formData):string{
     return returnString
 }
 
+
 export async function POST(request:NextRequest){
     let requestBody:formData = await request.json()
     console.log(requestBody)
-    const messageConfig = {
+    const incomingConfig = {
         from:process.env.EMAIL_NAME,
-        to:requestBody.email,
-        subject:"Contact Message from Abhinav Parbhakar Portfolio",
+        to:process.env.EMAIL_NAME,
+        subject:"Contact information from Portfolio form",
         text:createEmailText(requestBody)
     }
 
+    const outgoingConfig = {
+        from:process.env.EMAIL_NAME,
+        to:requestBody.email,
+        subject:"Contact Message from Abhinav Parbhakar Portfolio",
+        text:"       Confirmation Email         \n\nThis is confirmation of your email to me, Abhinav Parbhakar.\nI will reach out to you as soon possible, thank you for reaching out to me\n\nRegards,\nAbhinav Parbhakar",
+        html:"<div style='display: flex; flex-direction: column; align-items: center; max-width: 800px;'><div style='display: flex; justify-content: center; background-color: rgb(203 213 225); max-height: 96px;'><h1 style='color: rgb(251 146 60);'><strong>Email Confirmation</strong></h1></div><div style='display: flex; flex-direction: column; margin-top: 1rem;'><p>This is confirmation of your email to me, Abhinav Parbhakar. I will reach out to you as soon possible. Thank you for reaching out to me.</p><br /><p>Regards,</p><p>Abhinav</p></div></div>"
+    }
+
     try {
-        await transporter.sendMail(messageConfig)
-        return NextResponse.json({message:"Email sent succesfully"})
+        await transporter.sendMail(incomingConfig)
+        await transporter.sendMail(outgoingConfig)
+        return NextResponse.json({message:"Email(s) sent succesfully"})
     } catch (error) {
         return NextResponse.json({message:"Something went wrong"})
     }
