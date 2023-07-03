db45333efc2b62a83fa94badab35a3f34f5c38ad
Abhinav parbhakar <abhinav.parbhakar302@gmail.com>
2023-07-03 02:13:49 -0600

    adding the contact me form'

--- abhinav_parbhakar_site/app/Contact/page.tsx
+++ abhinav_parbhakar_site/app/Contact/page.tsx
@@ -1,7 +1,9 @@
 export default function Contact(){
     return (
-    <div className="flex flex-col">
-
+    <div className="flex flex-col mt-4 items-center">
+        <h1 className="flex">Contact Me </h1>
+        <title>Contact Me</title>
+        <h2 className="flex">Please fill out the following form:</h2>
     </div>
     )
 }
